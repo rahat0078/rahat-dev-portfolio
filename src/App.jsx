a9d110625946bfc374e30bc28e9491ecedf20173
rahat0078 <rarahat1710@gmail.com>
2025-01-04 22:59:21 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import Banner from "./components/Banner"
 import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="font-Roboto">
-      <header className="bg-[#212643] min-h-screen">
+      <header className="bg-[#212643]">
         <Navbar></Navbar>
+        <Banner/>
       </header>
+
+
     </div>
   )
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,33 @@
+import { Typewriter } from "react-simple-typewriter";
+import profile from '../assets/profile.png'
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto py-16 text-white flex flex-col sm:flex-row gap-4 justify-between items-center px-6 lg:px-4">
+            <div className="space-y-2 md:space-y-3 lg:space-y-4 flex-1">
+                <span className="text-xl md:text-2xl">Hello, I&apos;m</span>
+                <h1 className="text-3xl md:text-4xl lg:text-6xl font-semibold"> Ruhul Amin Rahat</h1>
+                <h3 className="text-xl md:text-2xl lg:text-4xl">
+                    A Passionate{' '}
+                    <span className="font-bold text-[#555bff]">
+                        {/* Style will be inherited from the parent element */}
+                        <Typewriter
+                            words={['FrontEnd Web Developer', 'Web Designer', 'MERN Stack Developer',]}
+                            loop={5}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={70}
+                            deleteSpeed={50}
+                            delaySpeed={800}
+                        />
+                    </span>
+                </h3>
+                <p className="w-full md:w-3/4 text-start">I&apos;m Rahat, a Frontend-Focused MERN Stack Developer passionate about creating user-centric web applications and always curious to explore new technologies. Let’s build something amazing together!</p>
+            </div>
+            <div className="lg:max-w-xl sm:max-w-lg max-w-sm flex-1">
+                <img className="border-4 border-[#555bff] rounded-full w-full" src={profile} alt="" />
+            </div>
+
         </div>
     );
 };
